39e3de32618f508829850eb9799cc922b9cb0742
João Luiz João <jlps0467@gmail.com>
2023-12-01 15:28:35 -0300

    Cadastrar Usuário

--- JS/scriptLogin.js
+++ JS/scriptLogin.js
@@ -53,28 +53,103 @@ function validacaoLogin() {
 
 function cadastrando_novo_user() {
     var nome = document.getElementById("nome").value;
-    var email = document.getElementById("email").value;
+    var n_email = document.getElementById("email").value;
     var nova_senha = document.getElementById("criando_senha").value;
     var senha = document.getElementById("senha").value;
+    var dt_nascimento = document.getElementById("date").value;
+    var sexo = document.querySelector('input[name="genero"]:checked');
+    var n_celular = document.getElementById("n_celular").value;
 
     if (nome == "") {
         alert("Digite seu nome.");
         document.getElementById("nome").focus();
-    } else if (email == "") {
+
+    } else if (n_email == "") {
         alert("Coloque seu email.");
         document.getElementById("email").focus();
+
     } else if (nova_senha == "") {
         alert("Crie uma senha.");
         document.getElementById("criando_senha").focus();
+
     } else if (senha == "") {
         alert("Confirme a senha.");
         document.getElementById("senha").focus();
+
     } else if (nova_senha != senha) {
         alert("As senhas colocadas não são iguais.");
         document.getElementById("senha").focus();
-    } else {
-        
-        
 
+    } else if (dt_nascimento == "") {
+        alert("Informe a data de nascimento.");
+        document.getElementById("date").focus();
+
+    } else if (sexo == null) {
+        alert("Informe seu gênero.");
+
+    } else if (n_celular == "") {
+        alert("Informe o número de seu celular.");
+        document.getElementById("n_celular").focus();
+
+    } else {
+        sexo = sexo.value;
+
+        fetch('http://localhost:3000/getData')
+            .then(response => response.json())
+            .then(data => {
+
+                let x = true
+                let count = 0
+                var emails = []
+                var cadastrar = true;
+
+                while (x) {
+                    try {
+                        emails.push(data[count].email);
+                        count += 1;
+                    } catch {
+                        x = false;
+                    }
+                }
+
+                for (let i = 0; i < emails.length; i++) {
+                    if (emails[i] == n_email) {
+                        alert("Este email já está cadastrado!");
+                        cadastrar = false
+                        break;
+                    }
+                }
+
+                if(cadastrar){
+                    var data = {
+                        nome: nome,
+                        email: n_email,
+                        senha: senha,
+                        dt_nascimento: dt_nascimento,
+                        sexo: sexo,
+                        n_celular: n_celular
+                    };
+    
+                    fetch('http://localhost:3000/cadastrar_user', {
+                        method: 'POST',
+                        headers: {
+                            'Content-Type': 'application/json'
+                        },
+                        body: JSON.stringify(data)
+                    })
+                        .then(response => response.json())
+                        .then(data => {
+                            console.log('Resposta:', data);
+                        })
+                        .catch(error => {
+                            console.error('Erro:', error);
+                        });
+    
+                    open('XGames.html');
+                    close('Cadastro_user.html');
+                }
+
+            })
+            .catch(error => console.error('Erro ao obter dados:', error));
     }
 }
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const sqlite3 = require('sqlite3').verbose();
+const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
 
@@ -14,6 +15,8 @@ app.use((req, res, next) => {
 
 app.use(express.static('public'));
 
+app.use(bodyParser.json());
+
 // Obter dados da tabela tb_user
 app.get('/getData', (req, res) => {
     db.all('SELECT * FROM tb_user', (err, rows) => {
@@ -26,24 +29,6 @@ app.get('/getData', (req, res) => {
     });
 });
 
-// Rota para obter a quantidade de objetos na tabela tb_user
-var contador = 0;
-
-app.get('/quantidade-objetos', (req, res) => {
-    const sqlQuery = 'SELECT COUNT(*) AS quantidade FROM tb_user';
-    db.get(sqlQuery, (err, row) => {
-        if (err) {
-            console.error(err.message);
-            res.status(500).json({ error: 'Erro interno do servidor.', details: err.message });
-            return;
-        }
-
-        contador = row.quantidade;
-
-        res.json({ quantidade: contador });
-    });
-});
-
 // Obter dados da tabela tb_jogos
 app.get('/getDataJogos', (req, res) => {
     db.all('SELECT * FROM tb_jogos', (err, rows) => {
@@ -53,6 +38,23 @@ app.get('/getDataJogos', (req, res) => {
             const data = rows;
             res.json(data);
         }
+    });
+});
+
+// Cadastras Usuários
+app.post('/cadastrar_user', (req, res) => {
+    const { nome, email, senha, dt_nascimento, sexo, n_celular } = req.body;
+
+    db.run('INSERT INTO tb_user (nome, email, senha, dt_nascimento, sexo, n_celular) VALUES (?, ?, ?, ?, ?, ?)', [nome, email, senha, dt_nascimento, sexo, n_celular], function (err) {
+        if (err) {
+            return res.status(500).json({ error: err.message });
+        }
+
+        // ID do objeto inserido
+        const objectId = this.lastID;
+
+        // Responder com os dados do objeto cadastrado
+        res.status(201).json({ id: objectId, nome, email, senha, dt_nascimento, adm: 0, sexo, n_celular });
     });
 });
 
